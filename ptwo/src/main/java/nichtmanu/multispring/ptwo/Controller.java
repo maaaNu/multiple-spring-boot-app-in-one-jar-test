2434c78143824580753895957b687ccf6231d3aa
maaaNu <hr.kniep@googlemail.com>
2018-02-21 11:27:31 +0100

    internalized properties

--- ptwo/src/main/java/nichtmanu/multispring/ptwo/Controller.java
+++ ptwo/src/main/java/nichtmanu/multispring/ptwo/Controller.java
@@ -1,13 +1,24 @@
 package nichtmanu.multispring.ptwo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("controller2")
+@PropertySource("classpath:two.properties")
 public class Controller {
+
+    @Value("${my.property}")
+    private String myProperty;
 
     @GetMapping("/project2")
     public String bonjourMonde() {
         return "Bonjour Monde";
+    }
+
+    @GetMapping("/propTest")
+    public String propTest() {
+        return myProperty;
     }
 }
--- ptwo/src/main/java/nichtmanu/multispring/ptwo/PtwoApplication.java
+++ ptwo/src/main/java/nichtmanu/multispring/ptwo/PtwoApplication.java
@@ -2,6 +2,9 @@ package nichtmanu.multispring.ptwo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class PtwoApplication {
